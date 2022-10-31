b5084b6669335ab4f3135d99d64e91abb00ee765
FilipShtrkovski <filip.strkovski13@gmail.com>
2022-10-31 21:08:44 +0100

    first commit

---
+++ Domashno1.java
@@ -0,0 +1,16 @@
+package domashno1;
+
+public class Domashno1 {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD OPP FIKT");
+		System.out.println("Filip Shtrkovski INKI874");
+
+	}
+
+}
+
+
+
